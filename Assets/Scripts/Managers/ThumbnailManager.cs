63979794e25ca8d57edf850ff18d48f53b36faa5
AUryeong <jykee7030@gmail.com>
2024-03-13 23:04:29 +0900

    Fix Thumbnail Path

--- Assets/Scripts/Managers/ThumbnailManager.cs
+++ Assets/Scripts/Managers/ThumbnailManager.cs
@@ -11,15 +11,13 @@ public class ThumbnailManager : MonoBehaviour
     [SerializeField] private RectTransform thumbnailParent;
 
     [SerializeField] private Button exitButton;
-    
+
     private void Awake()
     {
         exitButton.onClick.RemoveAllListeners();
         exitButton.onClick.AddListener(Reset);
         
-        GetArtWorks(new DirectoryInfo(Application.dataPath));
-        
-        Debug.Log(Application.dataPath);
+        GetArtWorks(new DirectoryInfo($"{Application.dataPath}/Thumbnail"));
     }
 
     private void Reset()
@@ -29,14 +27,13 @@ public class ThumbnailManager : MonoBehaviour
     
     private void GetArtWorks(DirectoryInfo dir)
     {
-        foreach (FileInfo fileInfo in dir.GetFiles())
+        foreach (var fileInfo in dir.GetFiles())
         {
             if (Path.GetExtension(fileInfo.FullName) != ".png") continue;
             
             Texture2D texture2D = new Texture2D(2, 2);
             texture2D.LoadImage(File.ReadAllBytes(fileInfo.FullName));
             Sprite value = Sprite.Create(texture2D, new Rect(0f, 0f, (float)texture2D.width, (float)texture2D.height), new Vector2(0f, 0f));
-            Debug.Log(value);
             string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileInfo.FullName);
             Instantiate(originThumbnail, thumbnailParent).Init(fileNameWithoutExtension, value);
         }
